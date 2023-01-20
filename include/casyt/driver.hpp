75db2e9e5d9d75c0f5181aaf6ce791b590a54f66
sauhd <12920537-sauhd@users.noreply.gitlab.com>
2023-01-21 00:08:44 +0300

    Add driver

---
+++ include/casyt/driver.hpp
@@ -0,0 +1,13 @@
+/*
+   driver.hpp - //lib/casyt.driver
+
+  Created at 2023-01-21 by @sauhd.
+  Licensed under the MPL-3.0 license.
+*/
+
+#pragma once
+
+namespace casyt {
+
+    int entry_point(int, char const **);
+}
---
+++ lib/casyt/driver.cpp
@@ -0,0 +1,16 @@
+/*
+   driver.cpp - //lib/casyt.driver
+
+  Created at 2023-01-21 by @sauhd.
+  Licensed under the MPL-3.0 license.
+*/
+
+#include <casyt/driver.hpp>
+
+#include <iostream>
+
+int casyt::entry_point(int argc, char const **argv) {
+    std::cout << "Hello, world!" << std::endl;
+
+    return 0;
+}
