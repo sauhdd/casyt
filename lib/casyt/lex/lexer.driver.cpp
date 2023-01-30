07b0945e8dc13dc9e3f32a2fa198bfaa38998b76
sauhd <12920537-sauhd@users.noreply.gitlab.com>
2023-01-30 17:08:28 +0300

    Add lexer driver

---
+++ lib/casyt/lex/lexer.driver.cpp
@@ -0,0 +1,23 @@
+/*
+   lexer.driver.cpp - //lib/casyt.lex
+
+  Created at 2023-01-30 by @sauhd.
+  Licensed under the MPL-3.0 license.
+ */
+
+#include <casyt/lex/lexer.hpp>
+#include <casyt/lex/lexer_token.hpp>
+
+#include <iostream>
+
+int main(int argc, char const **argv) {
+    // make a logger object
+
+    if (argc < 2) {
+        // use the logger object
+
+        return 1;
+    }
+
+    return 0;
+}
