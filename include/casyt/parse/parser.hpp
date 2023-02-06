091200ad2a5f6ccf0bd0d5c701d98ca3a25113e6
sauhd <12920537-sauhd@users.noreply.gitlab.com>
2023-02-06 19:14:12 +0300

    Add parser

---
+++ include/casyt/parse/parser.hpp
@@ -0,0 +1,17 @@
+/*
+   parser.hpp - //lib/casyt.parse
+
+  Created at 2023-02-06 by @sauhd.
+  Licensed under the MPL-3.0 license.
+ */
+
+#pragma once
+
+namespace casyt {
+
+    class parser {
+      protected:
+      public:
+        ~parser();
+    };
+}
---
+++ lib/casyt/parse/parser.cpp
@@ -0,0 +1,10 @@
+/*
+   parser.hpp - //lib/casyt.parse
+
+  Created at 2023-02-06 by @sauhd.
+  Licensed under the MPL-3.0 license.
+ */
+
+#include <casyt/parse/parser.hpp>
+
+casyt::parser::~parser() = default;
